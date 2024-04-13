5bf9d36b84372e5a4d771e9faa76fab885bdcd30
karthi234231 <jogikarthik123@gmail.com>
2024-04-13 22:15:35 +0530

    pushing all files

---
+++ Ecomerce/server/app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 8005;
+
+
+app.listen(port, () => {
+    console.log(`your server is running on port ${port} `);
+});
+
